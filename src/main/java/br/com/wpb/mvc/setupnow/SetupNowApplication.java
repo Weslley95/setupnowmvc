10d8ec6cf4b14ec73b653c319c225905f64919a7
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-06-03 21:20:35 -0300

    [INCLUDE] Projeto base

---
+++ src/main/java/br/com/wpb/mvc/setupnow/SetupNowApplication.java
@@ -0,0 +1,13 @@
+package br.com.wpb.mvc.setupnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetupNowApplication.class, args);
+	}
+
+}
