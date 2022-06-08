5e3d1d4137f77319b5d993bf049b2cb76518680c
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-06-07 23:07:07 -0300

    [INCLUDE & UPDATE] Incluido dependencia JPA e MySQL, conexao com BD

--- src/main/java/br/com/wpb/mvc/setupnow/controller/HomeController.java
+++ src/main/java/br/com/wpb/mvc/setupnow/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,17 +15,20 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    private EntityManager entityManager;
+
     @GetMapping("/home")
     public String home(Model model) {
-        var pedido = new Pedido();
-        pedido.setNomeProduto("Smart Watch Relógio Inteligente Mi Watch S1 - Xiaomi");
-        pedido.setUrlImagem("https://cdn.shopify.com/s/files/1/0543/3507/8599/products/S8ad16487b4144a268c1153edb239cd61W.png?v=1649735560");
-        pedido.setUrlProduto("https://www.midobr.com/collections/smartwatch/products/lancamento-smart-watch-relogio-inteligente-mi-watch-s1-xiaomi");
-        pedido.setDescricao("Lançamento");
+        //var pedido = new Pedido();
+        //pedido.setNomeProduto("Smart Watch Relógio Inteligente Mi Watch S1 - Xiaomi");
+        //pedido.setUrlImagem("https://cdn.shopify.com/s/files/1/0543/3507/8599/products/S8ad16487b4144a268c1153edb239cd61W.png?v=1649735560");
+        //pedido.setUrlProduto("https://www.midobr.com/collections/smartwatch/products/lancamento-smart-watch-relogio-inteligente-mi-watch-s1-xiaomi");
+        //pedido.setDescricao("Lançamento");
 
-        List<Pedido> pedidoList = Arrays.asList(pedido);
+        Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+        List<Pedido> pedidoList = query.getResultList();
+
         model.addAttribute("pedidoList", pedidoList);
-
         return "home";
     }
 }
--- src/main/java/br/com/wpb/mvc/setupnow/model/Pedido.java
+++ src/main/java/br/com/wpb/mvc/setupnow/model/Pedido.java
@@ -3,12 +3,19 @@ package br.com.wpb.mvc.setupnow.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 @Getter @Setter
 public class Pedido {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nomeProduto;
     private BigDecimal valorNegociado;
     private LocalDate dataEntrega;
