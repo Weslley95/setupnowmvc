bade09831b43503ec30e0d190692b16a6f063604
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-06-08 22:45:26 -0300

    [INCLUDE] Incluido docker e refatorado classes

--- src/main/java/br/com/wpb/mvc/setupnow/controller/HomeController.java
+++ src/main/java/br/com/wpb/mvc/setupnow/controller/HomeController.java
@@ -1,30 +1,23 @@
 package br.com.wpb.mvc.setupnow.controller;
 
 import br.com.wpb.mvc.setupnow.model.Pedido;
-import lombok.var;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @PersistenceContext
     private EntityManager entityManager;
 
     @GetMapping("/home")
     public String home(Model model) {
-        //var pedido = new Pedido();
-        //pedido.setNomeProduto("Smart Watch Relógio Inteligente Mi Watch S1 - Xiaomi");
-        //pedido.setUrlImagem("https://cdn.shopify.com/s/files/1/0543/3507/8599/products/S8ad16487b4144a268c1153edb239cd61W.png?v=1649735560");
-        //pedido.setUrlProduto("https://www.midobr.com/collections/smartwatch/products/lancamento-smart-watch-relogio-inteligente-mi-watch-s1-xiaomi");
-        //pedido.setDescricao("Lançamento");
-
         Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
         List<Pedido> pedidoList = query.getResultList();
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO pedido (nome_produto, valor_negociado, data_entrega, url_produto, url_imagem, descricao) VALUES (
+'Smart Watch Relógio Inteligente Mi Watch S1 - Xiaomi',
+NULL,
+NULL,
+'https://www.midobr.com/collections/smartwatch/products/lancamento-smart-watch-relogio-inteligente-mi-watch-s1-xiaomi',
+'https://cdn.shopify.com/s/files/1/0543/3507/8599/products/S8ad16487b4144a268c1153edb239cd61W.png?v=1649735560',
+'Lançamento');
