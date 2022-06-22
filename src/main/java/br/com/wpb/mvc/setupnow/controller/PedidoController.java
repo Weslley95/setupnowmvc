7fef8efc20b06378efc18a1fc35297623fa93e5b
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-06-22 19:28:08 -0300

    [INCLUDE FORMULARIO] Formulario para pagina de pedido

---
+++ src/main/java/br/com/wpb/mvc/setupnow/controller/PedidoController.java
@@ -0,0 +1,15 @@
+package br.com.wpb.mvc.setupnow.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/pedido")
+public class PedidoController {
+
+    @GetMapping("/formulario")
+    public String formulario() {
+        return "pedido/formulario";
+    }
+}
