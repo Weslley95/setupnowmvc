a9bb64df58a0447e9a8393b9fea6bbdd19ec5ee0
Weslley Bezerra <weslleybezerra95@gmail.com>
2022-06-23 14:53:37 -0300

    [UPDATE ROTAS] Incrementado rotas do status para pedido

--- src/main/java/br/com/wpb/mvc/setupnow/repository/PedidoRepository.java
+++ src/main/java/br/com/wpb/mvc/setupnow/repository/PedidoRepository.java
@@ -1,9 +1,13 @@
 package br.com.wpb.mvc.setupnow.repository;
 
 import br.com.wpb.mvc.setupnow.model.Pedido;
+import br.com.wpb.mvc.setupnow.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByStatusPedido(StatusPedido statusPedido);
 }
